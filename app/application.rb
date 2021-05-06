949ccc7ff98155813339f5480f4bc917df8070d1
Tomas Tobar <codetombomb@gmail.com>
2021-05-06 18:56:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,12 @@
 class Application
+  
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
+    if req.path.split('/')[1] == 'items'
+      resp.write 
+    end
     binding.pry 
     
     resp.finish
